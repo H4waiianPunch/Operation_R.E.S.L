717c5e123ce3c2c0662eff9ab7a66d9a19dc7114
h4waiianpunch <ryankevincleary@gmail.com>
2024-02-29 13:45:08 -0400

    Reading a file now works

---
+++ ReadFile.py
@@ -0,0 +1,10 @@
+# Using this to read a file using the open() function
+
+import time
+
+# Assigning the file to a variable
+file=open("C:\\Users\\ryank\\OneDrive\\Desktop\\CleanUp\\School\\Year2\\Semester 2\\Capstone\\Operation RESL\\Test Files\\ReadMe.txt")
+
+time.sleep(1)
+
+print(file.read())
