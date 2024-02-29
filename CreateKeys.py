f411849c45c98fd0ec4e361d1d5f10fb0064e932
h4waiianpunch <ryankevincleary@gmail.com>
2024-02-29 14:44:43 -0400

    Key creation

---
+++ CreateKeys.py
@@ -0,0 +1,22 @@
+# Creates private/public keys
+
+from Crypto.PublicKey import RSA
+
+# Generates private/public keys
+key = RSA.generate(2048)
+
+# Specifies the folder they're saved in
+save_dir = "C:\\Users\\ryank\\OneDrive\\Desktop\\CleanUp\\School\\Year2\\Semester 2\\Capstone\\Operation RESL\\Test Files\\"
+
+# Extract private and public keys
+private_key = key.export_key()
+public_key = key.public_key().export_key()
+
+# Save the keys to the file specified
+with open(save_dir + "private.pem", "wb") as private_file:
+    private_file.write(private_key)
+
+with open(save_dir + "public.pem", "wb") as public_file:
+    public_file.write(public_key)
+
+print("Keys saved here", save_dir)
